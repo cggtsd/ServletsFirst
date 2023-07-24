b662902767364a7a382936269316fe7fa394d4be
CGG <cggtsds59@gmail.com>
2023-07-24 17:36:27 +0530

    Commit: 24-07-2023

---
+++ src/main/java/cgg/servlets/RegisterServlet.java
@@ -0,0 +1,48 @@
+package cgg.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h2>welcome to Register Servlet</h2>");
+		
+		String name = req.getParameter("user_name");
+		String password = req.getParameter("user_password");
+		String email = req.getParameter("user_email");
+		String gender = req.getParameter("user_gender");
+		String course = req.getParameter("user_course");
+		String cond = req.getParameter("condition");
+		
+		if(cond!=null) {
+		if(cond.equals("checked")) {
+			out.println("<h2>Name:         "+name+"</h2>");
+			out.println("<h2>Password:         "+password+"</h2>");
+			out.println("<h2>Email:         "+email+"</h2>");
+			out.println("<h2>Gender:         "+gender+"</h2>");
+			out.println("<h2>Course:         "+course+"</h2>");
+			
+			//
+			//jdbc
+			//
+		}
+		}
+		else{
+			out.println("<h2>you have not accepted terms and conditions</h2>");
+		}
+	
+		
+	}
+
+}
